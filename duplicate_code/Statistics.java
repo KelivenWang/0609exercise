dc82771c1f5ef9f27c39441e2cf13410fe9f4cc1
1053003 <yoyorockman211043@gmail.com>
2019-05-25 16:22:35 +0800

    1053003
    Signed-off-by: 1053003 <yoyorockman211043@gmail.com>

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -2,30 +2,22 @@ package com.directi.training.codesmells.duplicatecode;
 
 public class Statistics
 {
+
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
+        double sum1 = 0.0;
         double average1;
+	double average2;
 
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
+	average1 = calculateSampleVariance(double[] array1);
+	average2 = calculateSampleVariance(double[] array2);
 
-        double sum2 = 0;
-        double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
-
-        return Math.abs(average1 - average2);
+	return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
+        double sum1 = 0.0;
         double average1;
         for (double element : elements) {
             sum1 += element;
